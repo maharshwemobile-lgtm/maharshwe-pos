ef80f1e5586025f72c5e49715f61439db6962add
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-12 17:52:47 +0630

    Expand service preview

--- src/ServicePreview.jsx
+++ src/ServicePreview.jsx
@@ -1,2 +1,2 @@
 import React from 'react';
-export default function ServicePreview(){return <div className="card"><h3>Service Preview</h3></div>}
+export default function ServicePreview(){return <div className="card"><h3>Service Preview</h3><p>Total 3</p><p>Done 3</p></div>}
