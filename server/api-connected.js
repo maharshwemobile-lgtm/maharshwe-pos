1c853fd7522c72373cb3ca41eab2f60ed6150929
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-12 22:01:06 +0630

    Switch connected API to hard DB tables

--- server/api-connected.js
+++ server/api-connected.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const { getDb } = require('./db');
-const attachPosDataApi = require('./pos-data-api');
+const attachHardDbApi = require('./hard-db-api');
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 const protect = (_req, _res, next) => next();
-app.get('/api/health', (_req, res) => res.json({ ok: true, server: 'api-connected' }));
-attachPosDataApi(app, { protect });
+app.get('/api/health', (_req, res) => res.json({ ok: true, server: 'hard-db-api' }));
+attachHardDbApi(app, { protect });
 
 const PORT = process.env.PORT || 4000;
-getDb().then(() => app.listen(PORT, () => console.log('API connected on :' + PORT)));
+getDb().then(() => app.listen(PORT, () => console.log('Hard DB API running on :' + PORT)));
