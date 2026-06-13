9ff733c3406a424865fd119491cc99479d725b66
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-13 10:35:56 +0630

    Attach CSV product import API

--- server/api-connected.js
+++ server/api-connected.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { getDb } = require('./db');
 const attachHardDbApi = require('./hard-db-api');
+const attachProductImportApi = require('./product-import-api');
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json({ limit: '50mb' }));
 const protect = (_req, _res, next) => next();
 app.get('/api/health', (_req, res) => res.json({ ok: true, server: 'hard-db-api' }));
 attachHardDbApi(app, { protect });
+attachProductImportApi(app, { protect });
 
 const PORT = process.env.PORT || 4000;
 getDb().then(() => app.listen(PORT, () => console.log('Hard DB API running on :' + PORT)));
