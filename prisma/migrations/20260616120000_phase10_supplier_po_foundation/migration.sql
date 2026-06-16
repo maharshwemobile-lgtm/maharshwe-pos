ae44a0908e1e515575552dcecca7637123bee9d9
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 17:59:38 +0630

    feat: add suppliers table header

--- prisma/migrations/20260616120000_phase10_supplier_po_foundation/migration.sql
+++ prisma/migrations/20260616120000_phase10_supplier_po_foundation/migration.sql
@@ -1,1 +1,9 @@
 -- Phase 10 supplier and purchase order foundation
+
+CREATE TABLE "suppliers" (
+  "id" UUID NOT NULL,
+  "shop_id" UUID NOT NULL,
+  "supplier_code" VARCHAR(30) NOT NULL,
+  "name" VARCHAR(180) NOT NULL,
+  CONSTRAINT "suppliers_pkey" PRIMARY KEY ("id")
+);
