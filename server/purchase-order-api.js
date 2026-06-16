725ba3c565d11ddb9fc113387fcfe94a18704870
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 22:30:19 +0630

    feat: attach connected sale and void endpoints

--- server/purchase-order-api.js
+++ server/purchase-order-api.js
@@ -2,11 +2,14 @@ const attachPurchaseOrderReadApi = require('./purchase-order-read-api');
 const attachPoCreateApi = require('./po-create-api');
 const attachPoApproveApi = require('./po-approve-api');
 const attachSaleReportApi = require('./sale-report-api');
+const attachSaleApiV2 = require('./sale-api-v2');
+const attachSaleV2VoidApi = require('./sale-v2-void-api');
 
 module.exports = function attachPurchaseOrderApi(app) {
   attachPurchaseOrderReadApi(app);
   attachPoCreateApi(app);
   attachPoApproveApi(app);
   attachSaleReportApi(app);
-  require('./sale-api-v2')(app);
+  attachSaleApiV2(app);
+  attachSaleV2VoidApi(app);
 };
