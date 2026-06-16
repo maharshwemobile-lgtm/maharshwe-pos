05ff49ba9989a10da01ad50f10e7dde0d0fb3a41
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 18:27:23 +0630

    feat: add PO create route validation

--- server/po-create-api.js
+++ server/po-create-api.js
@@ -1,3 +1,23 @@
-module.exports = function attachPoCreateApi(app) {
-  void app;
-};
+const crypto = require('crypto');
+const { Prisma } = require('@prisma/client');
+const { access, ApiError, createOrderSchema, parse, wrap } = require('./purchase-order-core');
+const { prisma, assertTablesReady, audit, nextOrderNumber } = require('./purchase-order-db');
+const { getOrderDetail } = require('./purchase-order-query');
+
+function attachPoCreateApi(app) {
+  app.post('/api/purchasing/orders', ...access.write, wrap(async (req, res) => {
+    await assertTablesReady();
+    const input = parse(createOrderSchema, req.body || {});
+    void input;
+    void res;
+    void crypto;
+    void Prisma;
+    void ApiError;
+    void prisma;
+    void audit;
+    void nextOrderNumber;
+    void getOrderDetail;
+  }));
+}
+
+module.exports = attachPoCreateApi;
