7596f7ab147d140f9271229c09783bdb9e168fdc
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 19:38:43 +0630

    feat: attach scalable sale report route

--- server/purchase-order-api.js
+++ server/purchase-order-api.js
@@ -1,9 +1,11 @@
 const attachPurchaseOrderReadApi = require('./purchase-order-read-api');
 const attachPoCreateApi = require('./po-create-api');
 const attachPoApproveApi = require('./po-approve-api');
+const attachSaleReportApi = require('./sale-report-api');
 
 module.exports = function attachPurchaseOrderApi(app) {
   attachPurchaseOrderReadApi(app);
   attachPoCreateApi(app);
   attachPoApproveApi(app);
+  attachSaleReportApi(app);
 };
