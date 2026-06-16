0ac63e9320339e17f07eb8366cae3c9a76d30606
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 23:51:56 +0630

    feat: add customer resolver

--- server/commerce-order-prepare.js
+++ server/commerce-order-prepare.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const core = require('./commerce-core');
+
+async function resolveCustomer(tx, shopId, customerInput) {
+  const name = core.clean(customerInput?.name);
+  const phone = core.clean(customerInput?.phone);
+  if (!name && !phone) return null;
+  let customer = phone ? await tx.customer.findFirst({ where: { shopId, phone } }) : null;
+  if (!customer && name) {
+    customer = await tx.customer.findFirst({ where: { shopId, name: { equals: name, mode: 'insensitive' } } });
+  }
+  if (!customer) return tx.customer.create({ data: { shopId, name: name || phone || 'Customer', phone } });
+  return tx.customer.update({
+    where: { id: customer.id },
+    data: { ...(name ? { name } : {}), ...(phone ? { phone } : {}) },
+  });
+}
+
+module.exports = { resolveCustomer };
