e5e89dbcbfa1e3e8a6f2edb5cb02a055b3a103b8
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-16 23:48:24 +0630

    feat: add commerce error and parser

--- server/commerce-core.js
+++ server/commerce-core.js
@@ -3,4 +3,18 @@ const { z } = require('zod');
 const { Prisma } = require('@prisma/client');
 const { prisma } = require('./prisma');
 
-module.exports = { crypto, z, Prisma, prisma };
+class CommerceError extends Error {
+  constructor(status, message, details = null) {
+    super(message);
+    this.status = status;
+    this.details = details;
+  }
+}
+
+function parse(schema, value, message = 'Invalid request') {
+  const result = schema.safeParse(value);
+  if (!result.success) throw new CommerceError(400, message, result.error.flatten().fieldErrors);
+  return result.data;
+}
+
+module.exports = { CommerceError, crypto, parse, z, Prisma, prisma };
