0282db59c2f12a05d7ba2448609056333aa0b0ba
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-18 22:11:56 +0630

    fix(settings): keep finance categories only in Project Settings

--- src/DashboardBusinessV5.jsx
+++ src/DashboardBusinessV5.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import DashboardBusinessV4 from './DashboardBusinessV4.jsx';
-import ExpenseCategoryPanel from './ExpenseCategoryPanel.jsx';
 
 export default function DashboardBusinessV5(props) {
-  return (
-    <>
-      <DashboardBusinessV4 {...props} />
-      <ExpenseCategoryPanel />
-    </>
-  );
+  return <DashboardBusinessV4 {...props} />;
 }
