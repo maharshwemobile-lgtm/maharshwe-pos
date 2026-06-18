b3d9cf1b648d3090dce7b42b404e6b5c7a8c9b55
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-18 20:13:36 +0630

    feat(pr23): inject dynamic wallet accounting before payment account routes

--- server/api-connected-pr23-v3.js
+++ server/api-connected-pr23-v3.js
@@ -1,10 +1,18 @@
 const originalAttachSalesPostgresApi = require('./sales-postgres-api');
+const originalAttachPaymentsAccountsPostgresApi = require('./payments-accounts-postgres-api');
 const attachPosSalePaymentMethodsV23 = require('./pos-sale-payment-methods-v23');
+const attachPaymentsAccountsDynamicV23 = require('./payments-accounts-dynamic-v23');
 
 const salesModulePath = require.resolve('./sales-postgres-api');
 require.cache[salesModulePath].exports = function attachSalesWithDynamicPaymentMethods(app) {
   attachPosSalePaymentMethodsV23(app);
   return originalAttachSalesPostgresApi(app);
+};
+
+const accountsModulePath = require.resolve('./payments-accounts-postgres-api');
+require.cache[accountsModulePath].exports = function attachAccountsWithDynamicPaymentMethods(app) {
+  attachPaymentsAccountsDynamicV23(app);
+  return originalAttachPaymentsAccountsPostgresApi(app);
 };
 
 require('./api-connected-pr23-v2');
