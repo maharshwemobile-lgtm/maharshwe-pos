1184a4234008a0e276b4ae0e09ff2eb64cacc481
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-19 08:23:11 +0630

    Load account management routes with Google auth module

--- server/account-management-loader-v24.js
+++ server/account-management-loader-v24.js
@@ -1,3 +1,10 @@
 const attachAccountManagementV24 = require('./account-management-v24');
+const originalGoogleAuthApi = require('./google-auth-api');
+
+const googleAuthModulePath = require.resolve('./google-auth-api');
+require.cache[googleAuthModulePath].exports = function attachGoogleAndAccountManagement(app) {
+  originalGoogleAuthApi(app);
+  attachAccountManagementV24(app);
+};
 
 module.exports = attachAccountManagementV24;
