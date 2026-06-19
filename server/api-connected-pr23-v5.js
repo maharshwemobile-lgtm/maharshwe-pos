d08441477be6a6ab5877e15bd8f4ddd0c6414435
maharshwemobile-lgtm <maharshwemobile@gmail.com>
2026-06-19 09:49:01 +0630

    Load configured AYA Pay and wallet payment methods

--- server/api-connected-pr23-v5.js
+++ server/api-connected-pr23-v5.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const projectSheetSettings = require('./google-sheet-project-settings-v23');
 const attachGoogleSheetProjectExportApi = require('./google-sheet-project-export-api-v23');
+require('./pos-payment-methods-loader-v24');
 
 const modulePath = require.resolve('./google-sheet-project-settings-v23');
 require.cache[modulePath].exports = {
